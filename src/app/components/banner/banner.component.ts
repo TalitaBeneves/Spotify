0e224426895860d1aa1eadf0776aeefd11d21804
Talita Beneves <talitabeneves15@gmail.com>
2022-02-26 02:16:39 -0300

    Criando banner de playlist

---
+++ src/app/components/banner/banner.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss']
+})
+export class BannerComponent implements OnInit {
+  @Input() ImagemUrl = '';
+  @Input() texto = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
