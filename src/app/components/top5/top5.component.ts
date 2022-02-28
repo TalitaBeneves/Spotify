8311e79355823f7dddd10623926fb17c307f67be
Talita Beneves <talitabeneves15@gmail.com>
2022-02-28 14:52:23 -0300

    top 5

--- src/app/components/top5/top5.component.ts
+++ src/app/components/top5/top5.component.ts
@@ -1,6 +1,7 @@
-import { IArtista } from './../../Interfaces/IArtista';
-import { SpotifyService } from 'src/app/services/spotify.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SpotifyService } from 'src/app/services/spotify.service';
+import { IArtista } from './../../Interfaces/IArtista';
 
 @Component({
   selector: 'app-top5',
@@ -11,7 +12,7 @@ export class Top5Component implements OnInit {
 
   artistas: IArtista[] = []
 
-  constructor(private spotifyService: SpotifyService) { }
+  constructor(private spotifyService: SpotifyService, private router: Router) { }
 
   ngOnInit(): void {
     this.buscarTopArtistas();
@@ -20,6 +21,10 @@ export class Top5Component implements OnInit {
   async buscarTopArtistas(){
     this.artistas = await this.spotifyService.buscarTopArtistas();
     console.log(this.artistas)
+  }
+
+  irParaPlaylist(artistaId: string){
+    this.router.navigateByUrl(`player/lista/artista/${artistaId}`)
   }
 
 }
